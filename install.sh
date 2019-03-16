814a3d2645ca6d7d9d48e7955bc5d64c3430c5ef
Rico <rico93@outlook.com>
2019-03-16 19:12:07 +1100

    docker 增加acme.sh

--- install.sh
+++ install.sh
@@ -416,6 +416,7 @@ install_docker(){
     echo "Start Docker "
     service docker start
     echo "Start Docker-Compose "
+    docker-compose pull
     docker-compose up -d
     echo
     echo -e "Congratulations, V2ray server install completed!"
@@ -472,6 +473,7 @@ install_dependencies(){
 
         yum_depends=(
              curl
+             socat
         )
         for depend in ${yum_depends[@]}; do
             error_detect_depends "yum -y install ${depend}"
@@ -479,6 +481,7 @@ install_dependencies(){
     elif check_sys packageManager apt; then
         apt_depends=(
            curl
+           socat
         )
         apt-get -y update
         for depend in ${apt_depends[@]}; do
@@ -488,7 +491,8 @@ install_dependencies(){
     echo -e "[${green}Info${plain}] Setting TimeZone to Shanghai"
     ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
     date -s "$(curl -sI g.cn | grep Date | cut -d' ' -f3-6)Z"
-
+    echo -e "[${green}Info${plain}] Downloading acme.sh"
+    curl https://get.acme.sh | sh
 }
 #update_image
 update_image_v2ray(){
@@ -534,6 +538,7 @@ update_config_v2ray(){
     esac
 
     echo "Start Service"
+    docker-compose pull
     docker-compose up -d
 
 }
